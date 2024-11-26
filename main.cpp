38330a0667c03e1354781a4f20cbaa1420af0503
hasibul-hasib29 <hasibulhasibh@gmail.com>
2024-11-26 12:12:40 +0600

    changes occured

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+/*
+██╗  ██╗ █████╗ ███████╗██╗██████╗
+██║  ██║██╔══██╗██╔════╝██║██╔══██╗
+███████║███████║███████╗██║██████╔╝
+██╔══██║██╔══██║╚════██║██║██╔══██╗
+██║  ██║██║  ██║███████║██║██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═════╝ 
+*/
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define FASTIO ios ::sync_with_stdio(0); cin.tie(0);  cout.tie(0) ;
+const ll mod = 1e9+7;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int INF = 1e9;
+const ull LLINF = 4e18;
+const double EPS = 1e-9;
+void solve(){
+    cout<<"Hello World!"<<endl;
+}
+int main(){
+    FASTIO
+    ll t;
+    t=1;
+    cin>>t;
+    while (t--)
+    {
+    solve();
+    }
+    return 0;
+}
